7d802461e548b5249c541ac6eb73d2f7e01044c4
Jkow21 <chx105621@student.chorzow.merito.pl>
2025-04-10 21:30:27 +0200

    Create ExpenseControllers.cs

---
+++ ExpenseControllers.cs
@@ -0,0 +1,31 @@
+using BudgetBuddyAPI.Data;
+using BudgetBuddyAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetBuddyAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ExpensesController : ControllerBase
+{
+    private readonly BudgetContext _context;
+
+    public ExpensesController(BudgetContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Expense>> GetExpenses()
+    {
+        return Ok(_context.Expenses.ToList());
+    }
+
+    [HttpPost]
+    public ActionResult<Expense> AddExpense(Expense expense)
+    {
+        _context.Expenses.Add(expense);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(GetExpenses), new { id = expense.Id }, expense);
+    }
+}
