28562b16ba045fd44f779e594d5046267671fcb9
Jkow21 <chx105621@student.chorzow.merito.pl>
2025-04-10 21:31:33 +0200

    Create Expense.cs

---
+++ Expense.cs
@@ -0,0 +1,9 @@
+namespace BudgetBuddyAPI.Models;
+
+public class Expense
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public decimal Amount { get; set; }
+    public DateTime Date { get; set; }
+}
