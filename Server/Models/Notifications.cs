ae2718fff18118c96bcf2d8b0f3c15eaa0069084
Katarzyna Krawczyk <kasiakrawczyk999@gmail.com>
2025-06-04 14:34:12 +0200

    feat: Add Notifications model, service, and controller for user notifications management
    Signed-off-by: Katarzyna Krawczyk <kasiakrawczyk999@gmail.com>

---
+++ Server/Models/Notifications.cs
@@ -0,0 +1,11 @@
+﻿namespace BudgetBuddy.Models;
+
+public class Notifications {
+  public int Id { get; set; }
+  public string UserId { get; set; }
+  public User User { get; set; }
+  public string Message { get; set; }
+  public DateTime CreatedAt { get; set; }
+  public DateTime? RemindAt { get; set; }
+  public bool IsRead { get; set; }
+}
